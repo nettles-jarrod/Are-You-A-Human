d8c281ff4bb424b5e5f7c666c619c33642654008
Favio Manriquez Leon <favio@favrik.com>
2013-02-27 16:47:57 -0800

    Update from ayah.php library version 1.1.7
    * Update session secret variable name
    * Update getPublisherHTML()
    * Update recordConversion()
    * Update $fields URL encoding in doHttpsPost()

--- src/AYAH/AYAH.php
+++ src/AYAH/AYAH.php
@@ -39,8 +39,8 @@ class AYAH
 	 */
 	public function __construct($publisherKey, $scoringKey, $webServiceHost = 'ws.areyouahuman.com')
 	{
-		if(array_key_exists('sessionSecret', $_REQUEST)) {
-			$this->sessionSecret = $_REQUEST['sessionSecret'];
+		if(array_key_exists('session_secret', $_REQUEST)) {
+			$this->sessionSecret = $_REQUEST['session_secret'];
 		}
 	
 		// Throw an exception if the appropriate information has not been provided.
@@ -75,12 +75,52 @@ class AYAH
 	 * Returns the markup for PlayThru.
 	 * @return string
 	 */
-	public function getPublisherHTML()
-	{
-		$url = 'https://' . $this->webService . "/ws/script/" . urlencode($this->publisherKey);
-	
-		return "<div id='AYAH'></div><script src='". $url ."' type='text/javascript' language='JavaScript'></script>";
-	}
+  public function getPublisherHTML($config = array())
+  {
+    // Initialize.
+		$session_secret = "";
+		$fields = array('config' => $config);
+		$webservice_url = '/ws/setruntimeoptions/' . $this->publisherKey;
+
+		// If necessary, process the config data.
+		if ( ! empty($config))
+		{
+			// Add the gameid to the options url.
+			if (array_key_exists("gameid", $config))
+			{
+				$webservice_url .= '/' . $config['gameid'];
+			}
+		}
+
+		// Call the webservice and get the response.
+		$resp = $this->doHttpsPostReturnJSONArray($this->webService, $webservice_url, $fields);
+		if ($resp)
+		{
+			// Get the session secret from the response.
+			$session_secret = $resp->session_secret;
+
+			// Build the url to the AYAH webservice.
+			$url = 'https://';						// The AYAH webservice API requires https.
+			$url.= $this->webService;				// Add the host.
+			$url.= "/ws/script/";						// Add the path to the API script.
+			$url.= urlencode($this->publisherKey);			// Add the encoded publisher key.
+			$url.= (empty($session_secret))? "" : "/".$session_secret;	// If set, add the session_secret.
+
+			// Build and return the needed HTML code.
+			return "<div id='AYAH'></div><script src='". $url ."' type='text/javascript' language='JavaScript'></script>";
+		}
+		else
+		{
+			// Build and log a detailed message.
+			$url = "https://".$this->webService.$webservice_url;
+			$message = "Unable to connect to the AYAH webservice server.  url='$url'";
+
+			// Build and display a helpful message to the site user.
+			$style = "padding: 10px; border: 1px solid #EED3D7; background: #F2DEDE; color: #B94A48;";
+			$message = "Unable to load the <i>Are You a Human</i> PlayThru&trade;.  Please contact the site owner to report the problem.";
+			echo "<p style=\"$style\">$message</p>\n";
+		}
+  }
 	
 	/**
 	 * Check whether the user is a human. Wrapper for the scoreGame API call
@@ -92,8 +132,10 @@ class AYAH
 		
 		if($this->sessionSecret)
 		{
-			$fields = array('sessionSecret' 	=> urlencode($this->sessionSecret),
-							'scoring_key' 		=> $this->scoringKey);
+			$fields = array(
+        'session_secret' => urlencode($this->sessionSecret),
+				'scoring_key'    => $this->scoringKey
+      );
 			
 			$resp = $this->doHttpsPostReturnJSONArray($this->webService, '/ws/scoreGame', $fields);
 			
@@ -113,11 +155,11 @@ class AYAH
 	 */
 	public function recordConversion()
 	{
-		if(isset($this->sessionSecret))
+		if (isset($this->sessionSecret))
 		{
 			return '<iframe style="border: none;" height="0" width="0" src="https://' .
 					$this->webService . '/ws/recordConversion/'.
-					urlencode($this->sessionSecret) . '"></iframe>';
+					urlencode($this->publisherKey) . '"></iframe>';
 		}
 		else
 		{
@@ -145,7 +187,7 @@ class AYAH
 	
 		return $result;
 	}
-	
+
 	/**
 	 * Initiate a request to the AYAH web service through curl or using a socket
 	 * if the system does not have curl available.
@@ -163,9 +205,13 @@ class AYAH
 		foreach($fields as $key => $value)
 		{
 			if (is_array($value)) {
-				foreach ($value as $v) {
-					$fields_string .= $key . '[]=' . $v . '&';
-				}
+        if (! empty($value)) {
+				  foreach ($value as $k => $v) {
+					  $fields_string .= $key . '['. $k .']=' . $v . '&';
+          }
+        } else {
+          $fields_string .= $key . '=&';
+        }
 			} else {
 				$fields_string .= $key.'='.$value.'&';
 			}
